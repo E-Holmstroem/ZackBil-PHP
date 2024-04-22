f21e4d8d58c8ca664a6bc9ef9b3f9e64f6254f83
E-Holstroem <05erihol@skola.boras.se>
2024-04-22 13:13:22 +0200

    PHPMailer konfigurerad

--- validate/validateSendEmail.php
+++ validate/validateSendEmail.php
@@ -54,5 +54,58 @@ if (mail($to, $subject, $message, $headers)) {
 }
 */
 
+if (isset($_SESSION['user_email'])) {
+    $userEmail = $_SESSION['user_email'];
+    $userName = $_SESSION['user_name'];
+} 
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../phpmailer/vendor/phpmailer/phpmailer/src/Exception.php';
+require '../phpmailer/vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require '../phpmailer/vendor/phpmailer/phpmailer/src/SMTP.php';
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+
+use PHPMailer\PHPMailer\SMTP;
+
+
+//Load Composer's autoloader
+require '../phpmailer/vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 1;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = '1anonymindivid@gmail.com';                     //SMTP username
+    $mail->Password   = 'pdht ssrd leih wcfd';                               //SMTP password
+    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('1anonymindivid@gmail.com', 'ZackBil');
+    $mail->addAddress($userEmail);     //Add a recipient
+    //Content
+
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Test Email';
+    $mail->Body    = 'Hej ' + $userName + ' du har nu blivit prenumererad till vårat nyhetsbrev!';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+/*
 header("Location: ../index.php");
         exit(); 
+*/
